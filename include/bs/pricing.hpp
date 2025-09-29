2ac108abc6c275d58d7f67906900ccdd9dd357a3
frank-suwen <suwenw2@outlook.com>
2025-09-29 05:14:36 +0000

    add public headers for pricing.hpp

--- include/bs/pricing.hpp
+++ include/bs/pricing.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "params.hpp"
+
 namespace bs {
-    double price_call(const Params&);
-    double price_put(const Params&);
-}
+    double price_call(const Params& p);
+    double price_put(const Params& p);
+} // namespace bs
